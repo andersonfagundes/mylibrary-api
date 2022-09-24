84dbfafeb199f796ef8ad23dcb9ff2ab3b0a470c
Anderson <anderson.fagundes4@gmail.com>
2022-09-24 21:25:58 +0100

    Created test to delete user type

--- src/test/java/com/andersonfagundes/mylibrary/repository/UserTypeRepositoryTest.java
+++ src/test/java/com/andersonfagundes/mylibrary/repository/UserTypeRepositoryTest.java
@@ -3,9 +3,12 @@ package com.andersonfagundes.mylibrary.repository;
 import com.andersonfagundes.mylibrary.domain.UserType;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
 
 @DataJpaTest
 @DisplayName("Test for User Type Respository")
@@ -34,6 +37,16 @@ class UserTypeRepositoryTest {
         Assertions.assertThat(userTypeSaved).isNotNull();
         Assertions.assertThat(userTypeSaved.getId()).isNotNull();
         Assertions.assertThat(userTypeUpdated.getType()).isEqualTo(userTypeSaved.getType());
+    }
+
+    @Test
+    @DisplayName("Delete remove user types when successful")
+    void delete_RemovesUserType_WhenSuccessfull(){
+        UserType userTypeToBeSaved = createUserType();
+        UserType userTypeSaved = this.userTypeRepository.save(userTypeToBeSaved);
+        this.userTypeRepository.delete(userTypeSaved);
+        Optional<UserType> userTypeOptional = this.userTypeRepository.findById(userTypeSaved.getId());
+        Assertions.assertThat(userTypeOptional).isEmpty();
     }
 
     private UserType createUserType() {
